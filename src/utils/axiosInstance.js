ab47c8fd0791adf42cc7ad0db126f98c6e9a034e
NAYAAB  AHMED N <nayaabahmedn@NAYAABs-MacBook-Air.local>
2025-01-12 14:43:32 +0530

    Add the netlify files

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:6002"
+    baseURL:"https://shortenurl-backend.onrender.com"
 })
 
 export default axiosInstance;
